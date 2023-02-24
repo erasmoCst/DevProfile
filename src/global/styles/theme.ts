42659bdd3816c378b4ecff5eb3a359a534cd2089
erasmo-costa <ecosta@viptech.com.br>
2023-02-24 18:58:41 -0300

    Feature: Styled components and global theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    black: '#000000',
+    white: '#ffffff',
+    primary: '#eba417',
+    primaryLight: '#ffdb77',
+    secondary: '#79c7ff',
+    success: '#00FF00',
+    danger: '#FF0000',
+    dark: '#122114',
+    light: '#f1f1f1',
+    gray500: '#a8a8b3',
+    gray800: '#29292e',
+  },
+};
