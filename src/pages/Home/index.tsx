e742889b08de217d783c7674dd8212fd07afb9d4
erasmo-costa <ecosta@viptech.com.br>
2023-02-27 18:55:15 -0300

    Feature: App Header with Icons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  Icon,
+  UseAvatarButton,
+  UserAvatar,
+  UserGreeting,
+  UserInfo,
+  UserInfoDetail,
+  UserName,
+  UserWrapper,
+} from './styles';
+
+import DefaultAvatar02 from '../../assets/avatar02.png';
+import { SafeAreaView } from 'react-native';
 
 export const Home = () => {
   return (
-    <Container>
-      <Title>Home Page</Title>
-    </Container>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Container>
+        <Header>
+          <UserWrapper>
+            <UserInfo>
+              <UseAvatarButton onPress={() => {}}>
+                <UserAvatar source={DefaultAvatar02} />
+              </UseAvatarButton>
+              <UserInfoDetail>
+                <UserGreeting>Olá,</UserGreeting>
+                <UserName>Erasus</UserName>
+              </UserInfoDetail>
+            </UserInfo>
+            <Icon name="power" />
+          </UserWrapper>
+        </Header>
+      </Container>
+    </SafeAreaView>
   );
 };
