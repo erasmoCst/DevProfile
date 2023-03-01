f9ba5a47970bae0bef1b627c6618ccf85dcb8731
erasmo-costa <ecosta@viptech.com.br>
2023-03-01 17:27:52 -0300

    Feature: Add component Logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import { LogoStyle } from './styles';
+
+export const Logo = () => {
+  return <LogoStyle source={logo} />;
+};
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,8 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+export const LogoStyle = styled.Image`
+  width: ${RFValue(160)}px;
+  height: ${RFValue(160)}px;
+  margin-bottom: ${RFValue(64)}px;
+`;
