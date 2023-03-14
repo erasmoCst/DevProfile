820cd5e222b5b7c579315702f19e64d3f246e30e
erasmo-costa <ecosta@viptech.com.br>
2023-03-14 14:21:30 -0300

    Feature(GestureHandler): add Gesture Handler and StatusBar

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
+import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
+import { Routes } from './src/routes';
+import { AuthProvider } from './src/context/AuthContext';
+import { StatusBar } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
-import AppLoading from 'expo-app-loading';
-import { NavigationContainer } from '@react-navigation/native';
-import { Routes } from './src/routes';
-import { AuthProvider } from './src/context/AuthContext';
 
 const App: React.FunctionComponent = () => {
   const [fontsLoaded] = useFonts({
@@ -22,13 +24,16 @@ const App: React.FunctionComponent = () => {
   }
 
   return (
-    <NavigationContainer>
-      <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
-      </ThemeProvider>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <StatusBar backgroundColor="trasparent" translucent />
+        <ThemeProvider theme={theme}>
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
+        </ThemeProvider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
